4e652d0f5792f74b53b330c69ee32b288076e3e5
LucaColazzo <74625867+LucaColazzo@users.noreply.github.com>
2023-09-23 02:23:32 +0200

    Update location.py

--- location.py
+++ location.py
@@ -4,5 +4,6 @@ import streamlit as st
 def render_location():
     st.title("Location")
     st.write("The wedding will take place at a beautiful venue.")
-    st.write("Address: 123 Wedding Lane, City, Country")
-    st.map(location=["123 Wedding Lane, City, Country"])
+    st.write("Via alla Madonnina, 22030, Barni, Italy")
+    st.map(location=["Via alla Madonnina, 22030, Barni, Italy"])
+    st.map(location=["Via alla Madonnina, 22030, Barni, Italy"])
