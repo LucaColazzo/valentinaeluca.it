827a60a0cfd6e8fd466d534595b4bdcea18f29ec
LucaColazzo <74625867+LucaColazzo@users.noreply.github.com>
2023-09-23 02:29:08 +0200

    Delete homepage.py

--- homepage.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-@st.experimental_memo
-def render_homepage():
-    st.title("Welcome to Valentina and Luca's Wedding Website")
-    st.write("We are excited to celebrate this special day with you!")
