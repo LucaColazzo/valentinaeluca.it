5905ffe63e534a82bc90cb45e403b84e787d154f
LucaColazzo <74625867+LucaColazzo@users.noreply.github.com>
2023-09-23 02:29:27 +0200

    Delete location.py

--- location.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-@st.experimental_memo
-def render_location():
-    st.title("Location")
-    st.write("The wedding will take place at a beautiful venue.")
-    st.write("Via alla Madonnina, 22030, Barni, Italy")
-    st.map(location=["Via alla Madonnina, 22030, Barni, Italy"])
-    st.map(location=["Via alla Madonnina, 22030, Barni, Italy"])
