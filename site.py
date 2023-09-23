520edbdd632ac25ed05c1c9ae4ad3cc284aecc4a
LucaColazzo <74625867+LucaColazzo@users.noreply.github.com>
2023-09-23 02:44:39 +0200

    Update site.py

--- site.py
+++ site.py
@@ -19,10 +19,22 @@ def location():
 #    st.map(location=["ia alla Madonnina, 22030, Barni, Italy"])
 
     # Embedded Google Maps map
-    map_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13376.58358882849!2d9.279663353971735!3d45.903857368447824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47842494b52cf5ed%3A0xefa8dd191f0d6360!2sYour%20Wedding%20Location!5e0!3m2!1sen!2sus!4v1568286161380!5m2!1sen!2sus"
-    map_html = f'<iframe width="100%" height="450" frameborder="0" style="border:0" src="{map_url}" allowfullscreen></iframe>'
-    st.components.v1.html(map_html, width=0)
+    # map_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13376.58358882849!2d9.279663353971735!3d45.903857368447824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47842494b52cf5ed%3A0xefa8dd191f0d6360!2sYour%20Wedding%20Location!5e0!3m2!1sen!2sus!4v1568286161380!5m2!1sen!2sus"
+    # map_html = f'<iframe width="100%" height="450" frameborder="0" style="border:0" src="{map_url}" allowfullscreen></iframe>'
+    # st.components.v1.html(map_html, width=0)
 #    st.markdown(f'<iframe width="800" height="600" frameborder="0" style="border:0" src="{map_url}" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+# Define the map's latitude and longitude
+    latitude = 45.903857368447824
+    longitude = 9.279663353971735
+
+    # Calculate the map width based on the screen width
+    screen_width = st.beta_get_query_params()["width"][0] if "width" in st.beta_get_query_params() else 0.9
+    map_width = int(screen_width * 800)  # You can adjust the scaling factor as needed
+
+    # Embedded Google Maps map with responsive width
+    map_url = f"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13376.58358882849!2d{longitude}!3d{latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47842494b52cf5ed%3A0xefa8dd191f0d6360!2sYour%20Wedding%20Location!5e0!3m2!1sen!2sus!4v1568286161380!5m2!1sen!2sus"
+    st.markdown(f'<iframe width="{map_width}" height="600" frameborder="0" style="border:0" src="{map_url}" allowfullscreen></iframe>', unsafe_allow_html=True)
 
 # RSVP
 def rsvp():
